92013e669215599083976cc3e66efd3339ecefc3
bmovo <2368591274@qq.com>
2022-08-23 08:56:53 +0800

    StringReverse

--- StringOperate/StringOperate.go
+++ StringOperate/StringOperate.go
@@ -1,7 +1,11 @@
 package StringOperate
 
-import "fmt"
-
-func StringReverse() {
-	fmt.Println("hello, world")
+func StringReverse(data string) string {
+	var mid []byte = []byte(data)
+	for i := 0; i < len(data); i++ {
+		for j := i + 1; j < len(data); j++ {
+			mid[i], mid[j] = mid[j], mid[i]
+		}
+	}
+	return string(mid)
 }
